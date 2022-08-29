be69ac3e0537a9dfccdd606cffb8c5a259404e4d
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-08-29 15:30:45 -0300

    comando cli para criar model

--- src/database/models/Users.js
+++ src/database/models/Users.js
@@ -1,22 +1,25 @@
 const Users = (sequelize, DataTypes) => {
     const Users = sequelize.define("Users", {
       id: {
-
+        type: DataTypes.INTERGER,
+        allowNull: false,
+        primaryKey: true,
       },
       displayName: {
-
+        type: DataTypes.STRING(255),
+        allowNull: false,
       },
-
       email: {
-
+        type: DataTypes.STRING(255),
+        allowNull: false,
       },
-
       password: {
-
+        type: DataTypes.STRING(255),
+        allowNull: false,
       },
       image: {
-        
-      }
+
+      },
 
 
     });
